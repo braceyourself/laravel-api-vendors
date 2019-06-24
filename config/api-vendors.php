cd0ad079774289bf02b5d10a0ff85a9b80c02425
Ethan Brace <ethanabrace@gmail.com>
2019-06-24 15:44:59 -0400

    Update api-vendors.php

--- config/api-vendors.php
+++ config/api-vendors.php
@@ -2,17 +2,6 @@
 
 return [
 
-    /*
-    'IEX' => [
-        'apiBasePath' => env('IEX_API_BASE_PATH', 'https://example.com'),
-        'endpoints' => [
-            'symbols' => "/symbols"
-        ]
-    ],
-
-    'JSONPlaceholder' => [
-        'apiBasePath' => env('JSONPLACEHOLDER_API_BASE_PATH', 'https://example.com'),
-    ],
-    */
+    
 
 ];
